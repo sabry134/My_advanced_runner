caaf713dd5f8c239bd3c119629d0b8af8fb54df5
Aurelien Duval <aurelien.duval@epitech.eu>
2023-07-23 16:26:31 +0200

    [ADD] multiplayer working

--- gui.cpp
+++ gui.cpp
@@ -4,9 +4,13 @@
 #include <algorithm>
 
 const int SHAPE_SPACING = 150;
+const float FRAME_WIDTH = 37.3f;
+const int FRAME_HEIGHT = 50;
+const int NUM_FRAMES = 11; // The number of frames in the animation
+const float ANIMATION_FRAME_TIME = 0.1f; // Time in seconds for each animation frame
 
 void open_window(std::vector<int>& clientSockets) {
-    sf::RenderWindow window(sf::VideoMode(800, 500), "SFML Shapes Window");
+    sf::RenderWindow window(sf::VideoMode(800, 500), "My advanced runner");
 
     sf::Texture backgroundTexture;
     if (!backgroundTexture.loadFromFile("img/runner_background.jpg")) {
@@ -26,6 +30,9 @@ void open_window(std::vector<int>& clientSockets) {
     // Create a vector to hold the client sprites
     std::vector<sf::Sprite> clientSprites;
 
+    int currentFrame = 0; // The current frame index in the animation
+    sf::Clock animationClock; // Clock for controlling animation frames
+
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
@@ -37,6 +44,12 @@ void open_window(std::vector<int>& clientSockets) {
                 sf::FloatRect visibleArea(0.f, 0.f, static_cast<float>(event.size.width), static_cast<float>(event.size.height));
                 window.setView(sf::View(visibleArea));
             }
+        }
+
+        // Update animation frames at a fixed frame rate
+        if (animationClock.getElapsedTime().asSeconds() >= ANIMATION_FRAME_TIME) {
+            currentFrame = (currentFrame + 1) % NUM_FRAMES;
+            animationClock.restart();
         }
 
         window.clear();
@@ -52,7 +65,7 @@ void open_window(std::vector<int>& clientSockets) {
 
         // Find the disconnected clients' sprites and remove them
         for (auto it = clientSprites.begin(); it != clientSprites.end();) {
-            if (std::distance(clientSprites.begin(), it) >= clientSockets.size()) {
+            if (static_cast<std::size_t>(std::distance(clientSprites.begin(), it)) >= clientSockets.size()) {
                 it = clientSprites.erase(it);
             } else {
                 ++it;
@@ -61,9 +74,12 @@ void open_window(std::vector<int>& clientSockets) {
 
         // Set the positions and draw the client sprites
         for (std::size_t i = 0; i < clientSprites.size(); ++i) {
+            // Custom X and Y positions (you can modify these values)
             float x = 100.0f + i * SHAPE_SPACING;
-            float y = 300.0f;
+            float y = 900.0f;
+
             clientSprites[i].setPosition(x, y);
+            clientSprites[i].setTextureRect(sf::IntRect(currentFrame * FRAME_WIDTH, 0, FRAME_WIDTH, FRAME_HEIGHT));
             window.draw(clientSprites[i]);
         }
 
